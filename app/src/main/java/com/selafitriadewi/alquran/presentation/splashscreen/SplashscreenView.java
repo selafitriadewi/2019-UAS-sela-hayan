b5f9017083ed1d269cba5f363f22bb50ef3e51f4
hayankhrisna <hayan.akk@gmail.com>
2019-05-26 12:51:57 +0700

    tambah splashscreen

--- app/src/main/java/com/selafitriadewi/alquran/presentation/splashscreen/SplashscreenView.java
+++ app/src/main/java/com/selafitriadewi/alquran/presentation/splashscreen/SplashscreenView.java
@@ -1,4 +1,9 @@
 package com.selafitriadewi.alquran.presentation.splashscreen;
 
 public interface SplashscreenView {
+    void onPrepare();
+
+    void onProgress(int progress);
+
+    void onSuccess();
 }
